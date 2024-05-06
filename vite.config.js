c45becf4b34dc49eec8050cd2d2d648a1963a53a
Jeff Nyman <jeffnyman@gmail.com>
2024-05-06 09:53:43 -0500

    Add overlord entry point.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/spec-workshop/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        overlord: resolve(__dirname, "pages/overlord.html"),
+      },
+    },
+  },
 });
