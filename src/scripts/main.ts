33ce2d39838544f23e0d8c1b3339f912c0667105
Jeff Nyman <jeffnyman@gmail.com>
2024-05-07 04:41:39 -0500

    Add logic to get provisioned codes.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,4 +4,20 @@ import $ from "jquery";
 
 $(document).ready(function () {
   console.log("Spec Workshop");
+
+  let activation_code: string;
+  let deactivation_code: string;
+  let countdown: string;
+
+  $("#provision-device").click(function () {
+    activation_code = $("#activation-value").val() as string;
+    deactivation_code = $("#deactivation-value").val() as string;
+    countdown = $("#countdown-value").val() as string;
+
+    console.log(`DEBUG: activation code: ${activation_code}`);
+    console.log(`DEBUG: deactivation code: ${deactivation_code}`);
+    console.log(`DEBUG: coutdown: ${countdown}`);
+
+    $("#provision-device").prop("disabled", true);
+  });
 });
