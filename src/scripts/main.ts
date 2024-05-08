143df6a9d27ae8b8e3c06a020c48354e5ba5f016
Jeff Nyman <jeffnyman@gmail.com>
2024-05-08 02:26:15 -0500

    Add validation to provision form.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,7 @@
 import "../styles/site.css";
 
 import $ from "jquery";
+import "jquery-validation";
 
 interface Trigger {
   resetDevice: () => void;
@@ -34,5 +35,59 @@ $(document).ready(function () {
 
   $("#reset").click(function () {
     trigger.resetDevice();
+  });
+
+  $("form[name='provision-device-form']").validate({
+    rules: {
+      activation_value: {
+        required: true,
+        digits: true,
+        minlength: 4,
+        maxlength: 4,
+      },
+      deactivation_value: {
+        required: true,
+        digits: true,
+        minlength: 4,
+        maxlength: 4,
+      },
+      countdown_value: {
+        required: true,
+        digits: true,
+        min: 1,
+        minlength: 1,
+        maxlength: 4,
+      },
+    },
+    messages: {
+      activation_value: {
+        required: "You must provide an activation code.",
+        digits: "The activation code must be numeric.",
+        minlength: "The activation code must be four numeric characters.",
+        maxlength:
+          "The activation code cannot be more than four numeric characters.",
+      },
+      deactivation_value: {
+        required: "You must provide a deactivation code.",
+        digits: "The deactivation code must be numeric.",
+        minlength: "The deactivation code must be four numeric characters.",
+        maxlength:
+          "The deactivation code cannot be more than four numeric characters.",
+      },
+      countdown_value: {
+        required: "A countdown value must be supplied.",
+        digits: "The countdown value must be a whole number.",
+        min: "The countdown value must be at least one second.",
+        minlength: "The countdown value must be a numeric value.",
+        maxlength: "The countdown value must be a numeric value.",
+      },
+    },
+  });
+
+  $("input").blur(function () {
+    if (!$(this).valid()) {
+      $(this).focus();
+      return false;
+    }
   });
 });
